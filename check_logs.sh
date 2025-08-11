cfd7193469b300a3d6a6d4c54ea68e2e82ae63b9
Dávid Balatoni <balcsida@gmail.com>
2025-08-11 20:49:31 +0200

    Remove debug scripts

--- check_logs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Monitoring NoQCNoLife logs..."
-echo "Please click on the menu bar icon to trigger detection"
-echo "Press Ctrl+C to stop"
-echo "================================================"
-
-log stream --process NoQCNoLife 2>&1 | grep "NoQCNoLife"
